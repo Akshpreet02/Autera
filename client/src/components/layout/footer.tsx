f3e3e4a52a26d9fad68cfa41dede8ddd50a3f470
Akshpreet02 <7312727-Akshpreet02@users.noreply.replit.com>
2025-03-30 03:39:59 +0000

    Restored to 'f76ea365deafd8a177bb5da6c11492714e649eae'
    Replit-Restored-To: f76ea365deafd8a177bb5da6c11492714e649eae

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8 xl:col-span-1">
-            <Link href="/" className="text-gray-800 font-bold text-xl">
+            <Link href="/" className="text-white font-bold text-xl">
               Autera
             </Link>
             <p className="text-muted text-base">
@@ -138,7 +138,7 @@ export function Footer() {
             </div>
           </div>
         </div>
-        <div className="mt-12 border-t border-gray-200 pt-8">
+        <div className="mt-12 border-t border-surface pt-8">
           <p className="text-base text-muted xl:text-center">
             &copy; {new Date().getFullYear()} Autera, Inc. All rights reserved.
           </p>
