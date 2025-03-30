c8b379f88b3ffecbf13e2ed4d62a713f796f8caa
Akshpreet Singh <akshpreet.singh.02@gmail.com>
2025-03-29 23:20:44 -0600

    netlify build

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,6 @@
+// netlify/functions/api.js
 const express = require('express');
 const serverless = require('serverless-http');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 // Import your routes
 const { registerRoutes } = require('../../server/routes');
@@ -9,8 +8,16 @@ const { registerRoutes } = require('../../server/routes');
 const app = express();
 
 // Middlewares
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // Use built-in middleware instead of body-parser
+app.use((req, res, next) => {  // Simple CORS middleware instead of cors package
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 // Register routes (without creating HTTP server)
 registerRoutes(app);
